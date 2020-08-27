26239ffc3c1392a213d910a0771b629d6e00237c
VinZ889 <68568640+VinZ889@users.noreply.github.com>
2020-08-27 11:18:22 +0800

    Get the items
    And places in inventory

---
+++ Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public InventoryObject Inventory;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        var item = other.GetComponent<Item>();
+        if (item)
+        {
+            Inventory.AddItem(item.item, 1);
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        Inventory.Container.Clear();
+    }
+}
