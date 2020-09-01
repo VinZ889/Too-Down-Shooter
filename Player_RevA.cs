818c5afc5aaf319d3d2e5ff2a61ce7245f9402e7
VinZ889 <68568640+VinZ889@users.noreply.github.com>
2020-09-01 23:15:23 +0800

    Player New Rev
    Addin Icon to Player
    Updated on 1_9_2020

---
+++ Player_RevA.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public InventoryObject Inventory;
+    private Sprite icon;
+
+    private void Awake()
+    {
+        //icon = GetComponent<Item>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        var item = other.GetComponent<ItemPickup>();
+        if (item)
+        {
+            Inventory.AddItem(item.item, 1, icon);
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        Inventory.list.Clear();
+    }
+}
