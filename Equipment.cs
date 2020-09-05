93d7667b4277467d39f9043d0d842abe6ce5e276
VinZ889 <68568640+VinZ889@users.noreply.github.com>
2020-09-05 11:27:42 +0800

    Equipment
    For the character to pick up the equipment, this needs to work with the Equipment Manager.
     therefore i have added an EquipmentManager

---
+++ Equipment.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory System/Equipment")]
+public class Equipment : Item
+{
+    public EquipmentSlot equipSlot;
+
+    public int AtkBonus;
+    public int DefBonus;
+
+    public override void Use()
+    {
+        base.Use();
+        {
+            base.Use();
+            EquipmentManager.instance.Equip(this);
+
+        }
+    }
+}
+
+public enum EquipmentSlot
+{
+    Head, Body, Leg, Weapon, glove, shield
+}
