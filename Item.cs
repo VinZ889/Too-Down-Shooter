7a0c5b4552f59c3e08df47b1a555ef4919c93f0a
VinZ889 <68568640+VinZ889@users.noreply.github.com>
2020-09-13 13:06:28 +0800

    New itemz
    Try and see if it works

--- Item.cs
+++ Item.cs
@@ -2,7 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum ItemType{ Hpotion, Potion}
 public class Item : MonoBehaviour
 {
-    public ItemObject item;
+    public ItemType type;
+    public Sprite spriteNeutral;
+    public Sprite spriteHighlighted;
+    public int maxSize;
+    public void Use()
+    {
+        switch (type)
+        {
+            case ItemType.Hpotion:
+                break;
+            case ItemType.Potion:
+                break;
+        }
+    }
 }
