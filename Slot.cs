2767fcb0ffa4e32ee65fed746b5d6cbd88c8bfd1
VinZ889 <68568640+VinZ889@users.noreply.github.com>
2020-09-13 13:07:08 +0800

    New Slot
    Try to see if it works

---
+++ Slot.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour, IPointerClickHandler
+{ 
+    private Stack<Item> items;
+    public Stack<Item> Items// Added after able to remove items by right click mouse
+    {
+        get { return items; }
+        set { items = value; }
+    }// Added after able to remove items by right click mouse
+
+    public Text stackTxt;
+    public Sprite slotEmpty;
+    public Sprite slotHighlight;
+
+    public bool IsEmpty
+    {
+        get { return items.Count == 0; }
+    }
+
+    public bool IsAvailable
+    {
+        get {return CurrentItem.maxSize > items.Count; }
+    }
+    public Item CurrentItem
+    {
+        get { return items.Peek();}
+    }
+
+    void Start()
+    {
+        items = new Stack<Item>();
+        RectTransform slotRect = GetComponent<RectTransform>();
+        RectTransform txtRect = stackTxt.GetComponent<RectTransform>();
+
+        int txtScaleFactor = (int)(slotRect.sizeDelta.x * 0.6);
+        stackTxt.resizeTextMaxSize = txtScaleFactor;
+        stackTxt.resizeTextMinSize = txtScaleFactor;
+
+        txtRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, slotRect.sizeDelta.y);
+        txtRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, slotRect.sizeDelta.x);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddItem(Item item)
+    {
+        items.Push(item);
+        if (items.Count >1)
+        {
+            stackTxt.text = items.Count.ToString();//check if we have items in stack
+        }
+        ChangeSprite(item.spriteNeutral, item.spriteHighlighted);
+    }
+
+    public void AddItems(Stack<Item> items)// Added after i tested can remove items using right mouse
+    {
+        this.items = new Stack<Item>(items);
+        stackTxt.text = items.Count > 1 ? items.Count.ToString() : string.Empty;
+        ChangeSprite(CurrentItem.spriteNeutral, CurrentItem.spriteHighlighted);
+    }// Added after i tested can remove items using right mouse(This steps 
+
+    private void ChangeSprite(Sprite neutral, Sprite highlight)
+    {
+        GetComponent<Image>().sprite = neutral;
+        SpriteState st = new SpriteState();
+        st.highlightedSprite = highlight;
+        st.pressedSprite = neutral;
+
+        GetComponent<Button>().spriteState = st;
+    }
+
+    private void UseItem()// change after invenotry slot able to count up
+    {
+        if (!IsEmpty)
+        {
+            items.Pop().Use();
+            stackTxt.text = items.Count > 1 ? items.Count.ToString() : string.Empty;
+
+            if (IsEmpty)
+            {
+                ChangeSprite(slotEmpty, slotHighlight);
+
+                Inventory.EmptySlots++;
+            }
+        }
+    }// change after invenotry slot able to count up
+
+    public void ClearSlot()// this is added to do  swap
+    { 
+        items.Clear();
+        ChangeSprite(slotEmpty, slotHighlight);
+        stackTxt.text = string.Empty;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)// this is added when i included IPointerClickHandler next to MonoBehaviour,
+    {
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            UseItem();
+        }
+    }
+}
