38896fea7513a528be8d454e3a5a83176c735ecd
VinZ889 <68568640+VinZ889@users.noreply.github.com>
2020-09-17 16:51:35 +0800

    First Draft Ammo
    This is the first draft for my Ammo

---
+++ Ammo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum ammo { type1, type2, type3}
+
+public class Ammo : Item
+{
+    public ammo Atype;
+}
