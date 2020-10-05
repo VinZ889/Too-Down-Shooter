6060ccff4cfc2adf96afc506ff2d3183400bc6e5
VinZ889 <68568640+VinZ889@users.noreply.github.com>
2020-10-05 10:42:52 +0800

    Gold Collected
    I have added the Gold gain in this script
    Please take a look

---
+++ GoldCollect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoldCollect : MonoBehaviour
+{
+    int goldAmount;
+    Text goldText;
+
+    void Start()
+    {
+        goldText = GetComponent<Text>();
+        goldText.text = goldAmount.ToString();
+    }
+
+    public void GoldGain(int goldGain)
+    {
+        goldAmount = goldAmount + goldGain;
+        goldText.text = goldAmount.ToString();
+    }
+}
