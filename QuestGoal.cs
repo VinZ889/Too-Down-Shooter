655055d2cd925177731c32e471d16ab1d8a1d2ea
VinZ889 <68568640+VinZ889@users.noreply.github.com>
2020-10-25 09:41:40 +0800

    Quest goal uploaded on 25 Oct
    Quest goal uploaded on 25 Oct

---
+++ QuestGoal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class QuestGoal 
+{
+    public GoalType goalType;
+    public int Requirement;
+    public int currentEarn;
+
+    public bool IsReached()
+    {
+        return (currentEarn >= Requirement);
+    }
+
+    public void EnemyDestroy()
+    {
+        if (goalType == GoalType.Kill)
+        currentEarn++;
+    }
+
+    public void Gathering()
+    {
+        if (goalType == GoalType.Gathering)
+            currentEarn++;
+    }
+}
+
+public enum GoalType
+{
+    Gathering,
+    Kill
+}
